d5f9c1c129c38f9d9a8bb9b14b3308f0a3565f5d
Nate Walters <newsproutsmedia@gmail.com>
2018-09-15 22:28:01 -0700

    Added Vets list and updated controller. Closes #15.

--- pet-clinic-web/src/main/java/com/newsproutsmedia/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/newsproutsmedia/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,26 @@
 package com.newsproutsmedia.sfgpetclinic.controllers;
 
+import com.newsproutsmedia.sfgpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/", "vets.html", "index.html", "index", ""})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
 
