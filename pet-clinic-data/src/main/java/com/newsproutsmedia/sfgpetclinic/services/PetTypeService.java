98b940b5cd8ddbb983aa739b2d932c5193b9f045
Nate Walters <newsproutsmedia@gmail.com>
2018-09-21 21:57:47 -0700

    create PetType map service. Closes #29.

---
+++ pet-clinic-data/src/main/java/com/newsproutsmedia/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package com.newsproutsmedia.sfgpetclinic.services;
+
+import com.newsproutsmedia.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+
+
+}
