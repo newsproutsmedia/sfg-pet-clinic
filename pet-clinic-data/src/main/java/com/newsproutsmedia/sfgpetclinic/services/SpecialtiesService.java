3883043949c00e28f06e9652839503f91802dfd8
Nate Walters <newsproutsmedia@gmail.com>
2018-09-22 14:47:34 -0700

    Created Specialties Map Service. Closes #31.

---
+++ pet-clinic-data/src/main/java/com/newsproutsmedia/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,8 @@
+package com.newsproutsmedia.sfgpetclinic.services;
+
+import com.newsproutsmedia.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+
+
+}
---
+++ pet-clinic-data/src/main/java/com/newsproutsmedia/sfgpetclinic/services/map/SpecialtiesMapService.java
@@ -0,0 +1,43 @@
+package com.newsproutsmedia.sfgpetclinic.services.map;
+
+import com.newsproutsmedia.sfgpetclinic.model.Specialty;
+import com.newsproutsmedia.sfgpetclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtiesMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+
+
+    @Override
+    public Set<Specialty> findAll() {
+
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+        super.deleteById(id);
+    }
+
+}
