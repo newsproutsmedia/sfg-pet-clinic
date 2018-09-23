66d397464d3d5f1a9a4c3e1d148880d303f2ecc8
Nate Walters <newsproutsmedia@gmail.com>
2018-09-22 21:39:30 -0700

    fixed menu links. Closes #45.

--- pet-clinic-data/src/main/java/com/newsproutsmedia/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/newsproutsmedia/sfgpetclinic/model/BaseEntity.java
@@ -1,16 +1,28 @@
 package com.newsproutsmedia.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    // GenerationType.IDENTITY indicates that the dB provider (MySQL, H2, etc) should determine the appropriate strategy for ID generation
+    // IDENTITY allows you to be more flexible in how IDs are created
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
     public Long getId() {
+
         return id;
     }
 
     public void setId(Long id) {
+
         this.id = id;
     }
 }
