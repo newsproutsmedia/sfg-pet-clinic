5b6a0c7d798f0b6f9f1ef017ca070d0192d0d10a
Nate Walters <newsproutsmedia@gmail.com>
2018-09-23 08:34:54 -0700

    Created Visit Map Service. Closes #46.

---
+++ pet-clinic-data/src/main/java/com/newsproutsmedia/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.newsproutsmedia.sfgpetclinic.services;
+
+import com.newsproutsmedia.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/newsproutsmedia/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,43 @@
+package com.newsproutsmedia.sfgpetclinic.services.map;
+
+import com.newsproutsmedia.sfgpetclinic.model.Visit;
+import com.newsproutsmedia.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        // make sure we have a proper object when saving
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
