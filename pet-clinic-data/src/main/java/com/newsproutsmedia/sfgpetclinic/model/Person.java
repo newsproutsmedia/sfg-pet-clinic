0b4c4eb9a74c2c32ad40bd5d6e356e8b59c70d41
Nate Walters <newsproutsmedia@gmail.com>
2018-09-22 21:49:51 -0700

    Completed mapping for owner entities. Closes #36.

--- pet-clinic-data/src/main/java/com/newsproutsmedia/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/newsproutsmedia/sfgpetclinic/model/Person.java
@@ -1,23 +1,34 @@
 package com.newsproutsmedia.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
 
+    @Column(name="first_name")
     private String firstName;
+
+    @Column(name="last_name")
     private String lastName;
 
     public String getFirstName() {
+
         return firstName;
     }
 
     public void setFirstName(String firstName) {
+
         this.firstName = firstName;
     }
 
     public String getLastName() {
+
         return lastName;
     }
 
     public void setLastName(String lastName) {
+
         this.lastName = lastName;
     }
 }
