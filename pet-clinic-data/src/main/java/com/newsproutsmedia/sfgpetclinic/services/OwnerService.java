412b39c842b0cfcfaca0e2933a81687a0ea5e1f4
Nate Walters <newsproutsmedia@gmail.com>
2018-10-03 13:45:52 -0700

    Fix Display Owner issue. Closes #59.

--- pet-clinic-data/src/main/java/com/newsproutsmedia/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/newsproutsmedia/sfgpetclinic/services/OwnerService.java
@@ -2,10 +2,12 @@ package com.newsproutsmedia.sfgpetclinic.services;
 
 import com.newsproutsmedia.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
 
     Owner findByLastName(String lastName);
 
-
+    List<Owner> findAllByLastNameLike(String lastName);
 }
