3952d5e68eb95819436fca5880909dccf616ecab
Nate Walters <newsproutsmedia@gmail.com>
2018-10-08 16:24:47 -0700

    Added PetControllerTest file with tests in it . #56.

---
+++ pet-clinic-web/src/test/java/com/newsproutsmedia/sfgpetclinic/controllers/PetControllerTest.java
@@ -0,0 +1,102 @@
+package com.newsproutsmedia.sfgpetclinic.controllers;
+
+import com.newsproutsmedia.sfgpetclinic.model.Owner;
+import com.newsproutsmedia.sfgpetclinic.model.PetType;
+import com.newsproutsmedia.sfgpetclinic.services.OwnerService;
+import com.newsproutsmedia.sfgpetclinic.services.PetService;
+import com.newsproutsmedia.sfgpetclinic.services.PetTypeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+public class PetControllerTest {
+
+    @Mock
+    PetService petService;
+
+    @Mock
+    OwnerService ownerService;
+
+    @Mock
+    PetTypeService petTypeService;
+
+    @InjectMocks
+    PetController petController;
+
+    MockMvc mockMvc;
+
+    Owner owner;
+    Set<PetType> petTypes;
+
+    @BeforeEach
+    void setUp() {
+        owner = Owner.builder().id(1L).build();
+
+        petTypes = new HashSet<>();
+        petTypes.add(PetType.builder().id(1L).name("Dog").build());
+        petTypes.add(PetType.builder().id(2L).name("Cat").build());
+
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(petController)
+                .build();
+
+    }
+
+    @Test
+    void initCreationForm() throws Exception {
+        // when the "findById" method is called with any Long value, return the owner object (with mock data)
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+
+        // when the "findAll" method is called, return petTypes with mock data
+        when(petTypeService.findAll()).thenReturn(petTypes);
+
+        // sed data via GET to /owners/1/pets/new
+        mockMvc.perform(get("/owners/1/pets/new"))
+                // expect that the page is reachable
+                .andExpect(status().isOk())
+                // expect that the model attribute of "owner" is available
+                .andExpect(model().attributeExists("owner"))
+                // expect that the model attribute of "pet" is available
+                .andExpect(model().attributeExists("pet"))
+                // expect that the page "pets/createOrUpdatePetForm" will be returned
+                .andExpect(view().name("pets/createOrUpdatePetForm"));
+    }
+
+    @Test
+    void processCreationForm() throws Exception {
+        // when the "findById" method is called with any Long value, return the owner object (with mock data)
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+
+        // when the "findAll" method is called, return petTypes with mock data
+        when(petTypeService.findAll()).thenReturn(petTypes);
+
+        // send data via POST to /owners/1/pets/new
+        mockMvc.perform(post("/owners/1/pets/new"))
+                // expect that the status is be ok
+                .andExpect(status().isOk())
+                // expect that the view name of redirect:/owners/1 will be returned
+                .andExpect(view().name("redirect:/owners/1"));
+
+        // check that a pet object was saved
+        verify(petService).save(any());
+
+    }
+
+}
